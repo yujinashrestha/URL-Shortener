46e1fdc2b692cd6663dc5429464e489b51c626ec
yujinashrestha <yujinashresths@gmail.com>
2026-02-05 01:41:34 +0545

    m

--- urlshort/api/index.py
+++ urlshort/api/index.py
@@ -1,5 +1,7 @@
-from django.core.wsgi import get_wsgi_application
 import os
+from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "urlshort.settings")
+
 application = get_wsgi_application()
