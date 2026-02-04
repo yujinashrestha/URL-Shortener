b2c01d2844f12c4b88ba6893fe6b078902dbb646
yujinashrestha <yujinashresths@gmail.com>
2026-02-05 01:36:47 +0545

    :m

--- urlshort/urlshort/settings.py
+++ urlshort/urlshort/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import whitenoise.runserver_nostatic
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,6 +34,7 @@ ALLOWED_HOSTS = ["*"]
 TAILWIND_APP_NAME = 'theme'
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
